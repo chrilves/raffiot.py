d49fbe05c2e0b73085d0ff97b4cbf21c90c53745
chrilves <christophe.calves@gmail.com>
2021-02-03 01:42:55 +0100

    v0.0.2

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
+version = "0.0.2"
+
 setup(
     name="raffiot",
     packages=["raffiot"],
-    version="0.0.1",
+    version=version,
     license="Apache 2.0",
     description="Robust And Fast Functional IO Toolkit",
     author="chrilves",
     author_email="calves@crans.org",
     url="https://github.com/chrilves/raffiot.py",
-    download_url="https://github.com/chrilves/raffiot.py/archive/v0.0.1.tar.gz",
+    download_url=f"https://github.com/chrilves/raffiot.py/archive/v{version}.tar.gz",
     keywords=["IO", "MONAD", "FUNCTIONAL PROGRAMMING"],
     classifiers=[
         "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
